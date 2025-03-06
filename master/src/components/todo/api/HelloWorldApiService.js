639e10b4580d003a26256f832a9fc56a93cf5066
Student Apnacollege <ayanckm@gmail.com>
2025-03-06 17:41:23 +0530

    Frontend Todo

---
+++ master/src/components/todo/api/HelloWorldApiService.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+import { apiClient } from "./apiClient";
+
+
+// export function retrieveHelloWorldBean(){
+//    return axios.get('http://localhost:8080/hello-world-bean')
+// }
+
+export const retrieveHelloWorldBean 
+    = () => apiClient.get('/hello-world-bean')
+
+
+//Response to preflight request doesn't pass access control check => Authorization header
+export const retrieveHelloWorldPathVariable
+    = (username, token) => apiClient.get(`/hello-world/path-variable/${username}`
+    )
